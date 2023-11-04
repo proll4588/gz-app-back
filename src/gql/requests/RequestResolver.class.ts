ea904bf58f02e064ac648e1372edd63a19ad76af
Kostya <prol4588@gmail.com>
2023-11-04 23:26:18 +0700

    add auth only method decorator

--- src/gql/requests/RequestResolver.class.ts
+++ src/gql/requests/RequestResolver.class.ts
@@ -1,9 +1,37 @@
+import { GraphQLError } from 'graphql';
 import { MyContext } from '../../apollo';
-import { ResolversFun } from '../type';
+import { ResolversFun, ResolversFunArgs } from '../type';
 
 export abstract class RequestResolver<Args, Return, Parent = unknown> {
   constructor() {}
 
-  abstract handle(args: Args, context?: MyContext): Return;
-  abstract resolve: ResolversFun<Args, Return, Parent>;
+  abstract handle(args: Args, context?: MyContext): Promise<Return>;
+  abstract resolve(...args: ResolversFunArgs<Args, Parent>): Promise<Return>;
 }
+
+export const authOnly = (
+  target: Object,
+  propertyName: string,
+  descriptor: TypedPropertyDescriptor<ResolversFun<any, any>>
+) => {
+  const fun = descriptor.value;
+
+  if (fun) {
+    descriptor.value = (...args: ResolversFunArgs<any>) => {
+      const userId = args[2].userId;
+      if (!userId) {
+        throw new GraphQLError('User is not authenticated', {
+          extensions: {
+            code: 'UNAUTHENTICATED',
+            http: { status: 401 },
+          },
+        });
+      }
+
+      const res = fun.apply(this, args);
+      return res;
+    };
+  }
+
+  return descriptor;
+};
